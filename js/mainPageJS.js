20ec9a5823e61f8248f00cf3b1f26e88c5db56c1
srikanthmannepalle <smannepalle@mobileiron.com>
2015-12-29 00:11:30 +0530

    FOPE6: added broken links label, made column adjustments

--- js/mainPageJS.js
+++ js/mainPageJS.js
@@ -5,6 +5,16 @@
 //    Scrollbar addition section
 if(typeof jQuery !="undefined") {
     $(document).ready(function () {
+        //var oTable = $('#playList').DataTable();
+        //pagination();
+        //
+        //function pagination(){
+        //    $('#playList').dataTable({
+        //        "sPaginationType":"full_numbers",
+        //        "aaSorting":[[2, "desc"]],
+        //        "bJQueryUI":true
+        //    });
+        //}
 
         //Star rating section
         //audio
@@ -62,64 +72,79 @@ if(typeof jQuery !="undefined") {
         });
 
 
-       //SetRatingStar();
-
-
-
-    //section to retrieve playlist Items
-    var channelName = 'TechGuyWeb';
-
-    $.get("https://www.googleapis.com/youtube/v3/channels", {
-            part: 'contentDetails',
-            forUsername: channelName,
-            key: 'AIzaSyAYVmp1339Gvva8j9Kc5HebekzTS2cLRa4'
-        },
-        function (data) {
-            $.each(data.items, function (i, item) {
-                console.log(item);
-                pid = item.contentDetails.relatedPlaylists.uploads;
-                getVids(pid);
-            })
-        }
-    );
-
-    function getVids(pid) {
-        $.get("https://www.googleapis.com/youtube/v3/playlistItems", {
-                part: 'snippet',
-                maxResults: 10,
-                playlistId: pid,
+        //section to retrieve playlist Items
+        var channelName = 'TechGuyWeb';
+        var index = 1;
+        $.get("https://www.googleapis.com/youtube/v3/channels", {
+                part: 'contentDetails',
+                forUsername: channelName,
                 key: 'AIzaSyAYVmp1339Gvva8j9Kc5HebekzTS2cLRa4'
             },
-
             function (data) {
-                var output;
-                var index = 1;
                 $.each(data.items, function (i, item) {
-                    //console.log(item);
-                    vidTitle = item.snippet.title;
-                    videoId = item.snippet.resourceId.videoId;
-                    //section to append
-                    output = '<tr><td>' + index + '</td><td class="titleCol">' + vidTitle + '</td><td>01:20:34 h</td><td>' +
-                        '<span col-xs-6 class="pull-left text-success">1000</span>' +
-                        '<span col-xs-6 class="pull-right text-danger">10</span>' +
-                        '</td><td><span col-xs-4 class="text-danger">2.0/5</span>' +
-                        '<span col-xs-4 class="text-warning qualityCenter">3.0/5</span>' +
-                        '<span col-xs-4 class="text-success">5.0/5</span></td></tr>';
-                    index += 1;
-                    console.log(output);
-                    //Append to results listStyleType
-                    $('#playlistItems').append(output);
+                    console.log(item);
+                    pid = item.contentDetails.relatedPlaylists.uploads;
+                    getVids(pid);
                 })
             }
         );
-    }
 
-    //Add scrollbar
-    $('#playList').DataTable({
-        "scrollY": "200px",
-        "scrollCollapse": true,
-        "paging": false
-        });
+        function getVids(pid) {
+
+            $.get("https://www.googleapis.com/youtube/v3/playlistItems", {
+                    part: 'snippet',
+                    maxResults: 10,
+                    playlistId: pid,
+                    key: 'AIzaSyAYVmp1339Gvva8j9Kc5HebekzTS2cLRa4'
+                },
+
+                function (data) {
+
+                    $.each(data.items, function (i, item) {
+                        console.log(item);
+                        videoId = item.snippet.resourceId.videoId;
+                        getStats(videoId,index);
+                        index += 1;
+                    })
+                }
+            );
+        }
+
+        //videoDuration
+        function getStats(videoID,index){
+            var output='';
+
+            $.get("https://www.googleapis.com/youtube/v3/videos?id="+videoID, {
+                    part: 'snippet,contentDetails,statistics,status',
+                    key: 'AIzaSyAYVmp1339Gvva8j9Kc5HebekzTS2cLRa4'
+                },
+                function (data) {
+                    $.each(data.items, function (i, item) {
+                        console.log(item);
+                        vidTitle = item.snippet.title;
+                        duration = item.contentDetails.duration;
+                        likes=item.statistics.likeCount;
+                        dislike=item.statistics.dislikeCount;
+                        comments=item.statistics.commentCount;
+
+                        output = '<tr><td data-sort-value='+index+'>'+index +'</td><td class="titleCol">' + vidTitle + '</td><td>'+duration.split("PT")[1]+'</td><td>' +
+                            '<span col-xs-6 class="pull-left text-success">'+likes+'</span>' +
+                            '<span col-xs-6 class="pull-right text-danger">'+dislike+'</span>' +
+                            '</td><td><span col-xs-4 class="text-danger" style="padding-left:25px;">2.0/5</span>' +
+                            '<span col-xs-4 class="text-warning qualityCenter">3.0/5</span>' +
+                            '<span col-xs-4 class="text-success">5.0/5</span>'+
+                            '<span class="glyphicon glyphicon-alert" aria-hidden="true"></span></td>'+
+                            '</tr>';
+
+                        //Append to results listStyleType
+                        $('#playlistItems').append(output);
+                        $("#playList").tablesorter({sortList: [[0,0]]});
+                    })
+
+                }
+
+            );
+        }
     });
 }
 
